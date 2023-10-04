ed8306a2be5dcd12d898f2201f735f8fc7af111e
Arjun Jagtap <107830192+arjun2499@users.noreply.github.com>
2023-10-04 04:19:29 -0700

    server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express=require('express');
 var app=express();
 app.use(express.static('public'))
-app.get("/",(req, res)=>{res.send("Welcome to  My Pradies");});
+app.get("/",(req, res)=>{res.send("Welcome to  My Paradise");});
 app.get("/product",(req, res)=>{var p={ "id":45, "title":"Gerbera","description":"Wedding Flower", "unitprice":14, "quatity":7000 };
                                        res.send(p);
 });
