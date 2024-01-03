cd8924935ef618831e566cb1e5bd142b8d911671
arjun2499 <arjunjagtap24699@gmail.com>
2024-01-03 20:30:38 +0530

    add

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ app.get("/",(req, res)=>{res.send("Welcome to  My Paradise");});
 app.get("/product",(req, res)=>{var p={ "id":45, "title":"Gerbera","description":"Wedding Flower", "unitprice":14, "quatity":7000 };
                                        res.send(p);
 });
-app.listen(6969);
+app.listen(7000);
 console.log("App is listening on port 7000");
