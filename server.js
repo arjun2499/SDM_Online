e44d34c9927524f32dd285d4b55112ce47c381f2
arjun2499 <arjunjagtap24699@gmail.com>
2024-01-03 20:13:55 +0530

    add

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express=require('express');
 var app=express();
-app.use(express.static('public'))
+app.use(express.static('Public'))
 app.get("/",(req, res)=>{res.send("Welcome to  My Paradise");});
 app.get("/product",(req, res)=>{var p={ "id":45, "title":"Gerbera","description":"Wedding Flower", "unitprice":14, "quatity":7000 };
                                        res.send(p);
 });
 app.listen(6969);
-console.log("App is listening on port 6969");
+console.log("App is listening on port 7000");
